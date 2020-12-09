36a5f4d710cc41b9b6cd833e0a8dfd49e569af1f
Thanh <tionguyen97@gmail.com>
2020-12-09 15:33:01 +0700

    deployment

--- rest-api-project/back-end/app.js
+++ rest-api-project/back-end/app.js
@@ -1,12 +1,15 @@
+const path = require('path');
+const fs = require('fs');
+const https = require('https');
+
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
-const http = require('http');
-const logger = require('morgan');
-
+const morgan = require('morgan');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const helmet = require('helmet');
+const compression = require('compression');
 
 const feedRouter = require('./routes/feed');
 const authRouter = require('./routes/auth');
@@ -14,23 +17,19 @@ const configs = require('./configs/configs');
 const socket = require('./utils/socket');
 
 const app = express();
-const server = http.createServer(app);
-const io = socket.init(server);
 
-io.on('connection', socket => {
-    console.log('Client connected');
-    io.emit('hello', 'bye');
-});
+// Config for HTTPS
+// const privateKey = fs.readFileSync('server.key');
+// const certificate = fs.readFileSync('server.cert');
 
-// CONNECT DB
-mongoose.set('useFindAndModify', false);
-mongoose.connect(configs.DB_CONNECT_STR, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(result => {
-        console.log('CONNECT DB SUCCESS!');
-    })
-    .catch(err => next(err));
+// 'a' means 'append' to access.log
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
-// app.use(logger('dev'));
+// init helmet
+app.use(helmet());
+// init compression
+app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -57,4 +56,26 @@ app.use((err, req, res, next) => {
     res.status(status).json({ message, data });
 });
 
-server.listen(process.env.PORT);
+
+// CONNECT DB
+mongoose.set('useFindAndModify', false);
+mongoose.connect(configs.DB_CONNECT_STR, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+        console.log('CONNECT DB SUCCESS!');
+
+        // Start listen when connect DB success
+        const server = app.listen(process.env.PORT || 8080);
+        // HTTPS
+        // const server = https.createServer({ key: privateKey, cert: certificate }, app);
+        // server.listen(process.env.PORT || 8080);
+
+        const io = socket.init(server);
+        io.on('connection', socket => {
+            console.log('Client connected');
+            io.emit('hello', 'bye');
+        });
+    })
+    .catch(err => next(err));
+
+// app.listen(port) ~~ const server = http.createServer(app);
+//               server.listen(port);

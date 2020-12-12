064ca708b23d63ba870ab7a371ed179a01f5459a
Thanh <tionguyen97@gmail.com>
2020-12-12 11:33:14 +0700

    remove folder public, refactor app.js

--- api-bootcamp/app.js
+++ api-bootcamp/app.js
@@ -1,8 +1,5 @@
-const path = require('path');
-
 require('dotenv').config();
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 
 const connectDB = require('./configs/db');
@@ -16,9 +13,6 @@ connectDB();
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Routers
 app.use('/api/v1/bootcamps', bootcampRouter);
